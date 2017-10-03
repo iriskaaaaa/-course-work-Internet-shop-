36c8492af703921e432fd92274d92e1680fa7d50
Iklinkova <iklinkova@yandex.ru>
2017-10-03 17:18:33 +0300

    commit

--- tourist-stories.ua/admin/delete_reviews.php
+++
@@ -1,9 +0,0 @@
-<?php
-   include("include/db_connect.php");
-   $id = $_GET['id_reviews'];
-   $id_story = $_GET['id_story'];
-   $id_user=$_GET['id_user'];
-   mysql_query(" DELETE FROM reviews WHERE id_reviews='$id' ");
-   mysql_close();
-   header("Location: reviews.php");
-?>
--- tourist-stories.ua/log/includes/db_connect.php
+++
@@ -1,9 +0,0 @@
-<?php
-$db_host= 'localhost';
-$db_user= 'root';
-$db_database ='tourist_stories_db';
-
-$link=mysql_connect('localhost','root');
-mysql_select_db('tourist_stories_db', $link) or die("Нет соединения с БД". mysql_error());
-
-?>
